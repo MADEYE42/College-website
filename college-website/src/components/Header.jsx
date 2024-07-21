d207fa80b44e77512e8c8256edb67cdde19bd224
Gouresh  Madye <117588160+MADEYE42@users.noreply.github.com>
2024-07-21 23:48:03 +0530

    Header done half

--- college-website/src/components/Header.jsx
+++ college-website/src/components/Header.jsx
@@ -2,22 +2,31 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className=''>
-        <div className='flex justify-between'>
+    <div className='bg-slate-50 text-blue-700 font-bold p-2' >
+        <div className='flex justify-between items-center'>
             <img src="https://vesit.ves.ac.in/navlogo.PNG" alt="logo"  width={300}/>
             <div>
-            <ul className='flex flex-col justify-center'>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
+            <ul className='flex gap-4 items-center p-2'>
+                <li className='hover:scale-110 duration-300'>Admission</li>
+                <li className='hover:scale-110 duration-300'>Autonomy</li>
+                <select name="info" id="" className='hover:outline-none bg-transparent' >
+                  <option value="">College Information</option>
+                  <option value="">Newsletters</option>
+                  <option value="">Facilities</option>
+                  <option value="">Gallery</option>
+                  <option value="">Blog</option>
+                </select>
+                <select name="info" id="" className='hover:outline-none bg-transparent' >
+                  <option value="">Ratings</option>
+                  <option value="">NAAC</option>
+                  <option value="">NBA</option>
+                  <option value="">NIRF</option>
+                  <option value="">AICTE</option>
+                  <option value="">PRME</option>
+                </select>
+                <li className='hover:scale-110 duration-300'>MHRD Initiative</li>
+                <li className='hover:scale-110 duration-300'>Contact Us</li>
+                <li className='bg-blue-800 text-white p-2 rounded-md hover:scale-105 duration-300'>Virtual Tour</li>
               </ul>
             </div>
               
