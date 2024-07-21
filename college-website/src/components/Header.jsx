9ffc50620873fc61c38833b548161bc688212c1b
Gouresh  Madye <117588160+MADEYE42@users.noreply.github.com>
2024-07-21 23:28:18 +0530

    initial

---
+++ college-website/src/components/Header.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className=''>
+        <div className='flex justify-between'>
+            <img src="https://vesit.ves.ac.in/navlogo.PNG" alt="logo"  width={300}/>
+            <div>
+            <ul className='flex flex-col justify-center'>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+              </ul>
+            </div>
+              
+        </div>
+    </div>
+  )
+}
+
+export default Header
